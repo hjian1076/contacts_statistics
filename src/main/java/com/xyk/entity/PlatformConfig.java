2fb20d76fa8d8ac75631aa3a2364ca7d0e957852
hjian1076 <1076167497@qq.com>
2018-04-25 00:30:07 +0800

    品牌

---
+++ src/main/java/com/xyk/entity/PlatformConfig.java
@@ -0,0 +1,53 @@
+package com.xyk.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table(name = "tb_platform_config", schema = "ms", catalog = "")
+public class PlatformConfig {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "platform_name")
+    private String platformName;//品牌名称
+    @Basic
+    @Column(name = "website")
+    private String website;//官网地址
+    @Basic
+    @Column(name = "create_time")
+    private Date createTime;//创建时间
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPlatformName() {
+        return platformName;
+    }
+
+    public void setPlatformName(String platformName) {
+        this.platformName = platformName;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
